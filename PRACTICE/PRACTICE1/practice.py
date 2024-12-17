a2c1c4eb6d6b2557cc2444afda5d289822f0a7d7
FAIZANAAZ779 <faixanaaz@gmail.com>
2024-12-17 18:04:26 +0500

    uodate

---
+++ PRACTICE/PRACTICE1/practice.py
@@ -0,0 +1,107 @@
+my_name :str= "faiza"
+print(my_name)
+print(type(my_name))
+full_name=f"{my_name} naaz"
+print(full_name)
+# paython me hm nam full_name snak case me hi deta he
+
+my_num :int=10.3
+# . waly decimale ki type yha hogi float
+print(my_num)
+print(type(my_num))
+
+# /////////////////////////
+is_running :bool=True
+print(is_running)
+print(type(is_running))
+
+# /////////////////////////
+num_1:int=10
+num_2:int=13
+print(num_1+num_2)
+# /////////////////////////
+# arrat
+namess=list = ["faiza","naaz"]
+# /////////////////////////
+
+# for absc in qsjj=
+#     print(my_name)
+#     print(absc)
+# /////////////////////////
+# promt engineer
+
+# promate (actinw)
+# persona (Acting)
+# task(what to do)
+# context (data->pdf-> text->excel)
+# format (pdf ->je=son->excel)
+
+# /////////////////////////
+num_3:int=10
+num_4:int=num_3+10
+print(num_3+num_4)
+
+# /////////////////////////
+# short hand method
+num_3 +=20
+print(num_3)
+# /////////////////////////
+num_7:float=10.232892
+print(
+    round(num_7)
+      
+      )
+# ismy wo 2 value dikha dega .ke bad wali
+print(
+    round(num_7,2)
+      
+      )
+
+# //////////////////////////
+
+name_me:str="Faiza Naaz"
+print(name_me.upper())
+print(name_me.lower())
+print(name_me.title())
+print(
+    len(name_me)
+    # ye length btay ga sath sath space bhi cout hoga
+)
+# //////////////////////////
+my_list =["faiza","naaz","areeba","honen"] 
+print(my_list)
+print(my_list[0])
+
+my_list.append("nawaz")
+print(
+   my_list 
+    # ye and me add krdega list ke
+      )
+
+my_list.pop()
+# list ke end me sy remove krdega
+print(my_list)
+
+delete_name=my_list.pop()
+# ye jisko delete kiya he os name ko return krwayga
+
+
+my_list.remove("faiza")
+# list ke faiza ko remove krdega ye name sy remove krta he
+print(my_list)
+
+# //////////////////////////
+# ye for loop he for in loop
+new_list :list =["faiza","areeba","faizan","shoaib","daniya"]
+
+for name in new_list:
+ print(name)
+ print("hello")
+#  ismy hga ye ke loop har ak value ko print to krwayga or nichy hmny jo helo likha he ye bhi loop ka part he wo phly name ko print krwayga or phir hellow phir dosra name phir hello 
+
+# ismy curly braket nhi hota oski jga hota he space matlb agr hm 1 space dengy or next word ko bhi nichy ak space dekr likhengy jis jis ko hm
+# blkl ak hi line me likhty jaygy ak space ke sath to wo loop ka part bnta jayga age hm bina space likhengy koch wo lopp sy bari chala jayga 
+# //////////////////////////
+for name in new_list:
+ print(name)
+print("hello")# jesy yha pe hello ka rint bina space ke liye he to yw loop mw cout nhi hoga
